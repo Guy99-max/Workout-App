fbf902b601a2fe46d07ff2e8bfdb7e615b497778
Guy99-max <ikaandsodi@gmail.com>
2025-06-08 23:49:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// src/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // מאפשר גישה מכל כתובת
-    port: 0,           // Vite יבחר פורט פנוי אוטומטית
-    strictPort: false, // אם הפורט לא פנוי, Vite יבחר פורט חלופי
+  root: './',  // מוודא ש-Vite יודע את המיקום של קובץ ה-HTML
+  build: {
+    outDir: 'dist',  // תיקיית ה-output לבנייה
   },
 });
